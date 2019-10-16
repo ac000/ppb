778f815d6152ffe842fd1f511fb1f37235a13b3a
Andrew Clayton <andrew@digital-domain.net>
2019-10-16 03:51:50 +0100

    Allow to read the bytes value from stdin
    This allows things like
    
        $ echo 1048576 | ./ppb
        1.00 MiB
    
    Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

--- ppb.c
+++ ppb.c
@@ -1,7 +1,7 @@
 /*
  * ppb.c - Pretty print bytes
  *
- * Copyright (C) 2015 - 2017	Andrew Clayton <andrew@digital-domain.net>
+ * Copyright (C) 2015 - 2019	Andrew Clayton <andrew@digital-domain.net>
  *
  * This software is released under the MIT License.
  * See MIT-LICENSE.txt
@@ -9,22 +9,30 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <libac.h>
 
 int main(int argc, char *argv[])
 {
 	ac_misc_ppb_t ppb;
+	char *bstring = argv[1];
 	int si_flags = AC_SI_UNITS_NO;
 	u64 bytes;
 
-	if (argc == 3) {
-		si_flags = AC_SI_UNITS_YES;
-	} else if (argc < 2) {
-		printf("Usage: ppb [--si] <bytes>\n");
+	if (argc == 2 && strcmp(argv[1], "-h") == 0) {
+		printf("Usage: ppb [-h] [--si] [bytes]\n");
 		exit(EXIT_FAILURE);
 	}
-	bytes = strtoll(argv[argc-1], NULL, 10);
+
+	if (argc > 1 && argv[1][0] == '-') {
+		si_flags = AC_SI_UNITS_YES;
+		bstring = argv[2];
+	}
+	if (argc == 3 || (argc == 2 && si_flags == AC_SI_UNITS_NO))
+		bytes = strtoll(bstring, NULL, 10);
+	else
+		scanf("%lu", &bytes);
 
 	ac_misc_ppb(bytes, si_flags, &ppb);
 	if (ppb.factor == AC_MISC_PPB_BYTES)
